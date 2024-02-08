fe46d5ffb033b050ff02becacc64ff598658a24f
prbrq <prbrq@ya.ru>
2024-02-08 22:54:20 +0300

    Add a database connection

--- Program.cs
+++ Program.cs
@@ -1,9 +1,31 @@
-﻿namespace SqlDatabaseUpdater;
+﻿using Microsoft.Extensions.Configuration;
+using Npgsql;
+
+namespace SqlDatabaseUpdater;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var builer = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+        var configuration = builer.Build();
+
+        var connectionString = configuration.GetConnectionString("Postgres");
+
+        var connection = new NpgsqlConnection(connectionString);
+        try
+        {
+            connection.Open();
+        }
+        catch (Exception ex) 
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        Console.Write("\nPress any key to close...");
+        Console.ReadKey(false);
     }
 }
